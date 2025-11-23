61b169c02acae92842873bbb2a268925968d5dbd
mahmud <mahmudhc1@gmail.com>
2025-11-23 15:34:14 -0500

    mahmud1 txt file added

---
+++ Javamain01.java
@@ -0,0 +1,28 @@
+package Practice01;
+
+public class Javamain01 {
+
+	public static void main(String[] args) {
+	
+		// 6.Method and constractor
+		                         // Constractor value / argument passing in constractor 
+		Student s1 = new Student("Mahmud",20,"mahmudhc1@gmail.com");
+		
+		
+	int score= 	s1.score(98, 92, 90); 
+	
+	System.out.println( "Total Score is : "+score);
+	System.out.println( "Total Score is Git Demo 1 : "+score);
+	System.out.println( "Total Score is Git Demo 2 : "+score);
+	
+	
+	
+	
+	
+
+	
+	
+
+	}
+
+}
